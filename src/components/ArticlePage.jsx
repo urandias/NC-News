05e0abe9d2204001f7d1045d1c8294e2115e6835
urandias <urandias@gmail.com>
2024-11-08 05:05:46 +0000

    -

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -22,6 +22,7 @@ function ArticlePage() {
             setLoading(false)
         })
         .catch((err) => {
+          console.log(err.message, '<<<<,')
           setError(err.message)
           setLoading(false)
         })
